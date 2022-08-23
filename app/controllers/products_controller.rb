9ee89ce70288386544bbfc0b4a4edbbc779d286b
Nguyen Khoa Hoang Phuc <nkhphuc@gmail.com>
2022-08-23 18:52:40 +0700

    product ajax button

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,11 +5,23 @@ class ProductsController < ApplicationController
 
     def create
         @drug.workplaces << current_workplace
+
+        respond_to do |format|
+            format.json { head :no_content }
+            format.js   { render :layout => false }
+         end
+
     end
 
     def destroy
         @product = current_workplace.products.find(params[:id])
         @product.destroy
+
+        respond_to do |format|
+            format.json { head :no_content }
+            format.js   { render :layout => false }
+         end
+
     end
 
     private
