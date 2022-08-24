ef422cd13b5b3f2f8efe5192d4ac0dbaed567536
Nguyen Khoa Hoang Phuc <nkhphuc@gmail.com>
2022-08-24 15:57:50 +0700

    email setup

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -5,7 +5,7 @@ ActionMailer::Base.smtp_settings = {
   :authentication       =>  :plain,
   :user_name            =>  'apikey',
   :password             =>  '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',
-  :domain               =>  'heroku.com',
+  :domain               =>  'nkhphuc@outlook.com',
   :enable_starttls_auto  =>  true
 }
 
