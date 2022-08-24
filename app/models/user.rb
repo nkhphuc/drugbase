0aea9987a0a777bcffee4033a6e5b817ba09a814
Nguyen Khoa Hoang Phuc <nkhphuc@gmail.com>
2022-08-24 14:50:23 +0700

    bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
   belongs_to :workplace
-  before_save :set_workplace
 
   attr_writer :login
   # Include default devise modules. Others available are:
@@ -38,12 +37,6 @@ class User < ApplicationRecord
     if User.where(email: username).exists?
       errors.add(:username, :invalid)
     end
-  end
-
-  private
-
-  def set_workplace
-    self.workplace_id = 1
   end
  
 end
