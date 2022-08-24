566eafc5eaa062b264d7a11c812a7cee2dc5565b
Nguyen Khoa Hoang Phuc <nkhphuc@gmail.com>
2022-08-24 16:31:54 +0700

    email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,8 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
-  #  config.action_mailer.default_url_options = { host: ENV['MAIL_HOST'] }
+  # config.action_mailer.default_url_options = { host: ENV['MAIL_HOST'] }
+  # config.action_mailer.default_url_options = { host: "glacial-reef-66399.herokuapp.com" }
   config.action_mailer.default_url_options = { host: "nkhphuc@outlook.com" }
   config.action_mailer.perform_deliveries = true
   
--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,9 +1,12 @@
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
-  :server               =>  'smtp.sendgrid.net',
-  :ports                =>  '587',
-  :username             =>  'apikey',
-  :password             =>  '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'
+  :server              =>  'smtp.sendgrid.net',
+  :ports                 =>  '587',
+  # :authentication       =>  :plain,
+  :user_name            =>  'apikey',
+  :password             =>  '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',
+  # :domain               =>  'heroku.com',
+  # :enable_starttls_auto  =>  true
 }
 
 
