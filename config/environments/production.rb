e2b59062a3d1859cb481298595e408b8e77388de
Nguyen Khoa Hoang Phuc <nkhphuc@gmail.com>
2022-08-24 14:41:29 +0700

    email bug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,12 +62,15 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "drugbase_production"
 
-  config.action_mailer.perform_caching = false
+  config.action_mailer.perform_caching = true
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
-
+  config.action_mailer.raise_delivery_errors = true
+  #  config.action_mailer.default_url_options = { host: ENV['MAIL_HOST'] }
+  config.action_mailer.perform_deliveries = true
+  
+  
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
