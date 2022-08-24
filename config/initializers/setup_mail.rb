facaa0fa3920f24099e873da4d9f538d4adbbe76
Nguyen Khoa Hoang Phuc <nkhphuc@gmail.com>
2022-08-24 16:35:39 +0700

    email

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,9 +1,9 @@
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
   :server              =>  'smtp.sendgrid.net',
-  :ports                 =>  '587',
+  :port                 =>  '587',
   # :authentication       =>  :plain,
-  :user_name            =>  'apikey',
+  :user_name            =>  'nkhphuc@gmail.com',
   :password             =>  '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',
   # :domain               =>  'heroku.com',
   # :enable_starttls_auto  =>  true
