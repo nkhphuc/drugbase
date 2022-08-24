186ab8798ae78e7fa6057d2e53be8200432a608c
Nguyen Khoa Hoang Phuc <nkhphuc@gmail.com>
2022-08-24 15:49:01 +0700

    email sengrid setting

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -3,8 +3,12 @@ ActionMailer::Base.smtp_settings = {
   :address              =>  'smtp.sendgrid.net',
   :port                 =>  '587',
   :authentication       =>  :plain,
-  :user_name            =>  'pp274560436@heroku.com',
-  :password             =>  'wp2q64zc3384',
+  :user_name            =>  'apikey',
+  :password             =>  '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',
   :domain               =>  'heroku.com',
   :enable_starttls_auto  =>  true
 }
+
+
+# 25, 587	(for unencrypted/TLS connections)
+# 465	(for SSL connections)
